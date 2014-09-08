f7a4ccfbfc45d9d3825aaa7ee2eb6dde0f302e45
Chris Hunt <cdhunt@gmail.com>
2014-09-08 14:33:36 -0400

    Initial

---
+++ newrelic_perfmon_plugin/Agent.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using NewRelic.Platform.Sdk;
+using System.Management;
+
+namespace newrelic_perfmon_plugin
+{
+    class PerfmonAgent : Agent
+    {
+        public override string Guid { get { return "com.automatedops.perfmom_plugin"; } }
+        public override string Version { get { return "0.0.1"; } }
+
+        private string Name { get; set; }
+        private List<Object> Counters { get; set; }
+        private ManagementScope ManagementScopt { get; set; }
+
+        public PerfmonAgent(string name, List<Object> paths)
+        {
+            Name = name;
+            Counters = paths;
+            ManagementScopt = new ManagementScope("\\\\" + Name + "\\root\\cimv2");      
+        }
+
+        public override string GetAgentName()
+        {
+            return Name;
+        }
+
+        public override void PollCycle()
+        {
+            foreach (Dictionary<string, Object> counter in Counters)
+            {
+
+                string providerName = counter["provider"].ToString();
+                string categoryName = counter["category"].ToString();
+                string counterName = counter["counter"].ToString();
+                string predicate = string.Empty;
+                if (counter.ContainsKey("instance"))
+                {
+                    predicate = string.Format(" Where Name = '{0}'", counter["instance"]);
+                }
+                string unitValue = counter["unit"].ToString();
+
+                string queryString = string.Format("Select Name, {2} from Win32_PerfFormattedData_{0}_{1}{3}", providerName, categoryName, counterName, predicate);
+                
+                ManagementObjectSearcher search = new ManagementObjectSearcher(ManagementScopt, new ObjectQuery(queryString));
+
+                ManagementObjectCollection queryResults = search.Get();
+
+                foreach (ManagementObject result in queryResults)
+                {
+                    float value = Convert.ToSingle(result[counterName]);
+                    string instanceName = string.Empty;
+
+                    if (result["Name"] != null)
+                    {
+                        instanceName = string.Format("({0})", result["Name"]);
+                    }
+
+                    string metricName = string.Format("{0}/{1}{3}/{2}", providerName, categoryName, counterName, instanceName);
+
+                    Console.WriteLine("{0}/{1}: {2} {3}", Name, metricName, value, unitValue);
+                    
+                    ReportMetric(metricName, unitValue, value);
+                    
+                }
+
+            }
+        }
+    }
+
+    class PerfmonAgentFactory : AgentFactory
+    {
+        public override Agent CreateAgentWithConfiguration(IDictionary<string, object> properties)
+        {
+            string name = (string)properties["name"];
+            List<Object> counterlist = (List<Object>)properties["counterlist"];
+
+            if (counterlist.Count == 0)
+            {
+                throw new Exception("'counterlist' is empty. Do you have a 'config/plugin.json' file?");
+            }
+
+            return new PerfmonAgent(name, counterlist);
+        }
+    }
+}
---
+++ newrelic_perfmon_plugin/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NewRelic.Platform.Sdk;
+using Topshelf;
+using Topshelf.Runtime;
+
+namespace newrelic_perfmon_plugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<PluginService>(sc =>
+                {
+                    sc.ConstructUsing(() => new PluginService());
+
+                    sc.WhenStarted(s => s.Start());
+                    sc.WhenStopped(s => s.Stop());
+                });
+                x.SetServiceName("newrelic_perfmon_plugin");
+                x.SetDisplayName("NewRelic Windows Perfmon Plugin");
+                x.SetDescription("Sends Perfmon Metrics to NewRelic Platform");
+                x.StartAutomatically();
+                x.RunAsPrompt();
+            });
+        }
+    }
+
+    class PluginService
+    {
+        Runner _runner;
+
+        public PluginService()
+        {
+            _runner = new Runner();
+            _runner.Add(new PerfmonAgentFactory());
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Starting service.");
+            try
+            {
+                _runner.SetupAndRun();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occurred, unable to continue.\n", e.Message);
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Stopping service in 5 seconds.");
+            System.Threading.Thread.Sleep(5000);
+            
+            _runner = null;
+        }
+    }
+}
